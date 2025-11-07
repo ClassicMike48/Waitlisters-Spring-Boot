8d0f3c358bd1e7d9e0f6305f2943a3bec09010c0
ClassicMike48 <71788173+ClassicMike48@users.noreply.github.com>
2025-11-07 15:22:38 -0600

    Added new Spring Boot backend file structure

---
+++ waitlist-backend-new/waitlisters/src/main/java/com/example/waitlisters/WaitlistersApplication.java
@@ -0,0 +1,13 @@
+package com.example.waitlisters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitlistersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitlistersApplication.class, args);
+	}
+
+}

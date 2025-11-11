3b26ebc5eb2527d55280c1c895c8b974fba8a2a1
ClassicMike48 <71788173+ClassicMike48@users.noreply.github.com>
2025-11-10 19:53:50 -0600

    pre-docker addition

--- waitlist-backend-new/waitlisters/src/main/java/com/example/waitlisters/controller/HomeController.java
+++ waitlist-backend-new/waitlisters/src/main/java/com/example/waitlisters/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.waitlisters;
+package com.example.waitlisters.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
